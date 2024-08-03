35743bc09c7abc5c14d326a2beda4d62477c8d58
Mitsuki1025 <141213109+Mitsuki1025@users.noreply.github.com>
2024-08-03 12:12:19 +0900

    Update test3.py

---
+++ test3.py
@@ -0,0 +1,129 @@
+#!/usr/bin/python3
+
+import pyrealsense2 as rs
+import numpy as np
+import math
+import cv2
+from ultralytics import YOLO
+import mediapipe as mp
+import time
+
+# define yolo model
+model = YOLO("yolov8n.pt")
+
+# ストリームの設定
+config = rs.config()
+config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+
+mp_pose = mp.solutions.pose
+pose = mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5)
+
+# ストリーミング開始
+pipeline = rs.pipeline()
+pipeline.start(config)
+
+# Alignオブジェクト生成
+align_to = rs.stream.color
+align = rs.align(align_to)
+
+try:
+    while True:
+        # フレーム待ち
+        frames = pipeline.wait_for_frames()
+
+        # RGBとDepthの位置ずれを修正
+        aligned_frames = align.process(frames)
+
+        # RGB
+        color_frame = aligned_frames.get_color_frame()
+        color_image = np.asanyarray(color_frame.get_data())
+
+        # 深度
+        depth_frame = aligned_frames.get_depth_frame()
+        depth_image = np.asanyarray(depth_frame.get_data())        
+
+        # 2次元データをカラーマップに変換
+        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.02), cv2.COLORMAP_JET)
+
+
+        results = model(color_image)
+
+        for r in results:
+            boxes = r.boxes
+
+            for box in boxes:
+                cls = int(box.cls[0])
+                if not (cls == 0):
+                    continue
+
+                # bounding box
+                x1, y1, x2, y2 = box.xyxy[0]
+                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
+
+                # put box in cam
+                cv2.rectangle(color_image, (x1, y1), (x2, y2), (255, 0, 255), 3)
+
+                # confidence
+                confidence = math.ceil((box.conf[0]*100))/100
+
+                # object details
+                org = [x1, y1]
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                fontScale = 1
+                color = (255, 0, 0)
+                thickness = 2
+
+                cv2.putText(color_image, 'Person', org, font, fontScale, color, thickness)
+
+                ### 画像処理 ###
+
+                # ポイントで指定した箇所の奥行きを計算
+                break
+                ### 画像処理 end ### 
+                
+        results = pose.process(color_image)
+                
+        if results.pose_landmarks:
+          print('SUCCESS LANDMARKS')
+          right_shoulder_y = results.pose_landmarks.landmark[12].y
+          left_shoulder_y = results.pose_landmarks.landmark[11].y
+          right_shoulder_x = results.pose_landmarks.landmark[12].x
+          left_shoulder_x = results.pose_landmarks.landmark[11].x
+
+          image_width, image_height = color_image.shape[1], color_image.shape[0]
+
+          right_shol_x = int(right_shoulder_x * image_width)
+          left_shol_x = int(left_shoulder_x * image_width)
+          right_shol_y = int(right_shoulder_y * image_height)
+          left_shol_y = int(left_shoulder_y* image_height)
+
+          px = (right_shol_x+left_shol_x)//2
+          py = (right_shol_y+left_shol_y)//2
+          
+          depth_data = depth_frame.get_distance(px, py)
+
+          # ポイントにマーカーと距離を表示
+          depth_str = str(round(depth_data, 2)) + "m"
+          cv2.drawMarker(color_image, (px,py), (0,0,255))
+          cv2.putText(color_image, depth_str, (px,py), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,255), thickness=2)
+          cv2.drawMarker(depth_colormap, (px,py), (0,0,255))
+          cv2.putText(depth_colormap, depth_str, (px,py), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,255), thickness=2)
+
+        # イメージの結合
+        images = np.hstack((color_image, depth_colormap))
+
+        # 表示
+        cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
+        cv2.imshow('RealSense', images)
+        
+        time.sleep(0.2)
+
+        # q キー入力で終了
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
+
+finally:
+    # ストリーミング停止
+    pipeline.stop()
